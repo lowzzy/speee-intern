ecfa2e224c0e29ff2acb1100816768f9419b9763
takumi.hiraoka <takumi.hiraoka@takumi-hiraoka.intra2.speee.local>
2021-09-12 15:48:14 +0900

    rubocopを走らせる

--- app/controllers/admin_users/sessions_controller.rb
+++ app/controllers/admin_users/sessions_controller.rb
@@ -1,28 +1,20 @@
 # frozen_string_literal: true
 
-class AdminUsers::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+module AdminUsers
+  class SessionsController < Devise::SessionsController
+    # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  def new
-    # redirect_to :root if user_signed_in?
-    super
+    # GET /resource/sign_in
+
+    # POST /resource/sign_in
+
+    # DELETE /resource/sign_out
+
+    # protected
+
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_sign_in_params
+    #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+    # end
   end
-
-  # POST /resource/sign_in
-  def create
-    super
-  end
-
-  # DELETE /resource/sign_out
-  def destroy
-    super
-  end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
