e33c27f1fff1ff7a29d8c98055962ebee068a388
AmiYamamot0 <otkm.amn@gmail.com>
2021-09-12 21:03:59 +0900

    会員登録（サインアップ）画面の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@
 
 class UsersController < ApplicationController
   def show; end
+
+  def user_params
+    params.permit(:name, :email, :tel, :address)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_110203) do
+ActiveRecord::Schema.define(version: 2021_09_12_114330) do
 
   create_table "admin_users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,6 +32,9 @@ ActiveRecord::Schema.define(version: 2021_09_11_110203) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.string "address", null: false
+    t.integer "tel", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
