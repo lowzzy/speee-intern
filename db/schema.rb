7a78b191bc0bc48bdf9194a155ae032254787a0a
lowzzy <mi0706chi@gmail.com>
2021-09-12 20:06:22 +0900

    conflict対応

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,18 @@
 
 ActiveRecord::Schema.define(version: 2021_09_12_073515) do
 
+  create_table "admin_users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
   create_table "properties", charset: "utf8mb4", force: :cascade do |t|
     t.integer "age"
     t.integer "prefecture"
@@ -23,18 +35,6 @@ ActiveRecord::Schema.define(version: 2021_09_12_073515) do
     t.integer "material_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-ActiveRecord::Schema.define(version: 2021_09_11_110203) do
-
-  create_table "admin_users", charset: "utf8mb4", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
