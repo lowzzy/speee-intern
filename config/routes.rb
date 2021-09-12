8c79c73f077e3d7cd8cd53c89a87d7483576d13b
AmiYamamot0 <otkm.amn@gmail.com>
2021-09-12 17:46:28 +0900

    重複しているコードを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
