d1feaee92789a3c72ffed22e943bbd995d4ffd57
AmiYamamot0 <otkm.amn@gmail.com>
2021-09-12 14:24:44 +0900

    user詳細ページでのsign_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
@@ -9,4 +10,6 @@ Rails.application.routes.draw do
     passwords: 'users/passwords',
     registrations: 'users/registrations'
   }
+  
+  resources :users, only: %i[show]
 end
