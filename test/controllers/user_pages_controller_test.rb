9a6fd15ada0d3aaf10d59175d3546569115a0cc2
takumi.hiraoka <takumi.hiraoka@takumi-hiraoka.intra2.speee.local>
2021-09-13 13:00:37 +0900

    いらないテストを削除.

--- test/controllers/user_pages_controller_test.rb
+++ test/controllers/user_pages_controller_test.rb
@@ -3,8 +3,8 @@
 require 'test_helper'
 
 class UserPagesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get home' do
-    get user_pages_home_url
-    assert_response :success
-  end
+  # test 'should get home' do
+  #   get user_pages_home_url
+  #   assert_response :success
+  # end
 end
