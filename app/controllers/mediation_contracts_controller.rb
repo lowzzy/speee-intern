75ff91476155399ca2801698566c34350670eead
ryo.sakata <ryo.sakata@ryo-sakata.intra2.speee.local>
2021-09-13 15:11:05 +0900

    一般媒介契約create

---
+++ app/controllers/mediation_contracts_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class MediationContractsController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    @mediation_contract = MediationContract.new
+  end
+
+  def create
+    @mediation_contract = MediationContract.new
+    @mediation_contract.user = current_user
+
+    if @mediation_contract.save
+      flash[:info] = '手続き継続に合意しました'
+      redirect_to user_pages_home_url
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     get 'question', to: 'admin_pages#question'
   end
 
+  resources :mediation_contracts, only: [:new, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
