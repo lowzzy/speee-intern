8ec2abce9950a52ad67a19d39f040d367ff3d5f7
AmiYamamot0 <otkm.amn@gmail.com>
2021-09-13 12:11:58 +0900

    Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-
-  #sign_upのときに、Viewからname, address, telの値を受け取る
+  # sign_upのときに、Viewからname, address, telの値を受け取る
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
