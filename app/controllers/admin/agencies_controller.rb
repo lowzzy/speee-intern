7da5ab1bbf039166fee36cfb68490c96147875a9
AmiYamamot0 <otkm.amn@gmail.com>
2021-09-15 16:45:23 +0900

    rubocop

--- app/controllers/admin/agencies_controller.rb
+++ app/controllers/admin/agencies_controller.rb
@@ -12,14 +12,13 @@ module Admin
 
     def create
       @agency = Agency.new(agency_params)
-      if @agency.save
-        # price画面で'登録が完了しました'と表示される
-        flash[:info] = '登録が完了しました'
-        redirect_to new_admin_agency_path
-      else
-        flash[:info] = "入力に誤りが含まれています : #{@agency.errors.full_messages.join('. ')}"
-        redirect_to new_admin_agency_path
-      end
+      flash[:info] = if @agency.save
+                       # price画面で'登録が完了しました'と表示される
+                       '登録が完了しました'
+                     else
+                       "入力に誤りが含まれています : #{@agency.errors.full_messages.join('. ')}"
+                     end
+      redirect_to new_admin_agency_path
     end
 
     def agency_params
