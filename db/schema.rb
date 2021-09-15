eb4a1940fd9a0f119fb605edc94a63ab0a2bc4ed
lowzzy <mi0706chi@gmail.com>
2021-09-15 17:49:57 +0900

    軽微な変更

--- db/schema.rb
+++ db/schema.rb
@@ -81,12 +81,6 @@ ActiveRecord::Schema.define(version: 2021_09_15_053231) do
     t.integer "user_id"
   end
 
-  create_table "towns", charset: "utf8mb4", force: :cascade do |t|
-    t.integer "city_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
