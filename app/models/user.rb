c70b3bcb0ecd1055a6460198aae0d26feff59238
lowzzy <mi0706chi@gmail.com>
2021-09-16 15:02:58 +0900

    belongs_to -> has_one

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
   }
 
   has_one :property
-  belongs_to :buy_contract, optional: true
+  has_one :buy_contract
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
