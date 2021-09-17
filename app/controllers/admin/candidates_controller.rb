b73456d5671a514ad34764c2530b11b72278c520
lowzzy <mi0706chi@gmail.com>
2021-09-17 10:17:15 +0900

    rubocop 対応

--- app/controllers/admin/candidates_controller.rb
+++ app/controllers/admin/candidates_controller.rb
@@ -7,7 +7,7 @@ module Admin
     def new
       @candidate_buyer = CandidateBuyer.new
       @offer = @candidate_buyer.build_offer
-      @users_name_id = User.pluck(:name,:id)
+      @users_name_id = User.pluck(:name, :id)
     end
 
     # Post 買い手候補登録
